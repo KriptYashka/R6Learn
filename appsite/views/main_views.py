caa74fa7f35c6fceb775170307abee16f7091917
Кучук Егор <kriptyashka@gmail.com>
2024-09-03 17:54:16 +0300

    BS5 added

--- appsite/views/main_views.py
+++ appsite/views/main_views.py
@@ -1,3 +1,8 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
 # Create your views here.
+def index_page(request: WSGIRequest):
+    template_name = "main/index.html"
+    context = {}
+    return render(request, template_name, context)
--- siegelearn/urls.py
+++ siegelearn/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from appsite.views import main_views, profile_views, register_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_views.index_page),
 ]
